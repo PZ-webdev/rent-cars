14fc6797f5746bc36837fed1c7893622480ca6c5
Radek Węgrzyniak <radekwegrzyniak49@gmail.com>
2022-06-02 20:32:26 +0200

    CarColors

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\CarColorController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -28,3 +29,4 @@ Route::resource('/archives', ArchivesController::class);
 Route::resource('/transactions', TransactionController::class);
 Route::resource('/cars', CarController::class);
 Route::resource('/users', UserController::class);
+Route::resource('/car-colors', CarColorController::class);
