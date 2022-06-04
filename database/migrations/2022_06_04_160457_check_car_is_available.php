1dda4a5d6f6c11f882fba4e50344eb5580909dca
Mechu2507 <mechu2507@gmail.com>
2022-06-04 18:15:50 +0200

    function

---
+++ database/migrations/2022_06_04_160457_check_car_is_available.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $function = "CREATE OR REPLACE FUNCTION check_car_is_available(
+            id		INT,
+            date_start   DATE
+         ) RETURNS BOOLEAN LANGUAGE plpgsql SECURITY DEFINER AS $$
+         
+         BEGIN
+         perform * from transactions 
+         where id_car = check_car_is_available.id 
+         and date_end < check_car_is_available.date_start; 
+            if not found then
+              return true;
+           else
+              return false;
+           end if;
+            
+         END;
+         $$;
+        ";
+  
+        DB::unprepared($function);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
